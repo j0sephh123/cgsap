dd80676e009cec6f5ac470ab3f399c323a0203ad
j0sephh123 <j0sephh@abv.bg>
2018-12-06 21:20:46 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,86 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+
+<div>
+  <div class="container">
+    <div class="box" id="1">
+      1
+    </div><br>
+    <div class="box" id="2">
+      2
+    </div><br>
+    <div class="box" id="3">
+      3
+    </div><br>
   </div>
+  <input @change="change" type="range" value=20 min="1" max="300">
+  <span @click="$store.commit('modal', {active: true})">{{range.value}}</span> 
+  <select v-model="select">
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+  </select>
+  <button @click="click()">Click</button>
+  <modal></modal>
+</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import ModalVue from './components/Modal.vue';
+
+export default {
+  components: {
+    modal: ModalVue,
+  },
+  data() {
+    return {
+      boxes: [
+        {id: 1, name: 'first' , active: true},
+        {id: 2, name: 'second', active: false},
+        {id: 3, name: 'third' , active: false},
+      ],
+      select: 1,
+      range: {
+        value: 20,
+        max: 300,
+      }
+      
     }
+  },
+  methods: {
+    change(e) {
+      this.range.value = +e.target.value;
+    },
+    click(){
+      this.moveRight();
+    },
+    moveRight() {
+      x.to(one, 0.5, {x: this.range.value});
+    },
+  },
+  watch: {
+    range: {
+      handler(v) {
+        console.log(v);
+      },
+      deep: true,
+    },
   }
+};
+</script>
+<style scoped>
+.box {
+  border: 1px solid black;
+  padding: 20px;
+  margin-left: 200px;
+  margin-top: 20px;
+  display: inline-block;
+  margin-bottom: 20px;
+}
+.active-box {
+  border: 5px solid black;
+  font-size: 20px;
+}
+.container {
+  border: 1px solid black;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    modal: {
+      active: false,
+    }
   },
   mutations: {
-
+    modal(state, bool) {
+      state.modal.active = bool;
+    },
   },
   actions: {
 
-  }
+  },
+  getters: {
+    modal: state => state.modal,
+  },
 })
